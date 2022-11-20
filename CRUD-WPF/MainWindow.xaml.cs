c1c2dcc246e5a2b43f7daf439c5eec57c0c2b18e
thales.targino <thales.targino@DSK-C69FPZ2>
2022-11-20 11:43:31 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD-WPF/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CRUD_WPF
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
