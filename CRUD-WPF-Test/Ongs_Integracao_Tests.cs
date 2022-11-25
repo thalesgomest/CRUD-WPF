c2b383c90e05d95919ab686dba1bedcf647d0283
Thales Gomes Targino <thales.targino@LUZ.INTRA>
2022-11-25 10:47:06 -0300

    feat: testes de integração pets

---
+++ CRUD-WPF-Test/Ongs_Integracao_Tests.cs
@@ -0,0 +1,92 @@
+﻿using CRUD_WPF.Interfaces;
+using CRUD_WPF.Model;
+using CRUD_WPF.Model.Databse;
+using Moq;
+using System.Collections.ObjectModel;
+
+namespace CRUD_WPF_Test
+{
+    public class Ongs_Integracao_Tests
+    {
+        private List<Ong> MockOngs()
+        {
+            List<Ong> output = new List<Ong>
+            {
+                new Ong
+                {
+                    Id = 1,
+                    Nome = "Ong 1",
+                   Endereco = "Endereço 1",
+                   Telefone = "99999999999",
+                    Email = "ong1@ong1",
+                },
+                new Ong
+                {
+                    Id = 2,
+                    Nome = "Ong 2",
+                    Endereco = "Endereço 2",
+                    Telefone = "99999999999",
+                    Email = "ong2@ong2",
+                },
+                new Ong
+                {
+                    Id = 3,
+                    Nome = "Ong 3",
+                    Endereco = "Endereço 3",
+                    Telefone = "99999999999",
+                    Email = "ong3@ong3",
+                }
+            };
+            return output;
+        }
+
+        [Fact]
+        public void BuscarOng()
+        {
+            Mock<IDatabase> mockDb = new Mock<IDatabase>();
+            mockDb.Setup(x => x.BuscaOngs()).Returns(MockOngs());
+
+            DataBaseGenerico conn = new DataBaseGenerico(mockDb.Object);
+
+            ObservableCollection<Ong> ListaOngs = new ObservableCollection<Ong>(conn.BuscaOngs());
+
+            var expected = MockOngs();
+            var actual = ListaOngs;
+
+            Assert.Equal(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Id, actual[i].Id);
+                Assert.Equal(expected[i].Nome, actual[i].Nome);
+                Assert.Equal(expected[i].Endereco, actual[i].Endereco);
+                Assert.Equal(expected[i].Telefone, actual[i].Telefone);
+                Assert.Equal(expected[i].Email, actual[i].Email);
+            }
+        }
+
+        [Fact]
+        public void CadastraOng()
+        {
+            {
+                Mock<IDatabase> mockDb = new Mock<IDatabase>();
+                mockDb.Setup(x => x.CadastraOng(It.IsAny<Ong>()));
+
+                DataBaseGenerico conn = new DataBaseGenerico(mockDb.Object);
+
+                Ong NovaOng = new Ong
+                {
+                    Id = 4,
+                    Nome = "Ong 4",
+                    Endereco = "Endereço 4",
+                    Telefone = "99999999999",
+                    Email = "ong4@ong4",
+                };
+
+                conn.CadastraOng(NovaOng);
+
+                mockDb.Verify(x => x.CadastraOng(It.IsAny<Ong>()), Times.Once);
+            }
+        }
+    }
+}
---
+++ CRUD-WPF-Test/Pets_Integracao_Tests.cs
@@ -0,0 +1,85 @@
+﻿using CRUD_WPF.Interfaces;
+using CRUD_WPF.Model;
+using CRUD_WPF.Model.Databse;
+using Moq;
+using System.Collections.ObjectModel;
+
+namespace CRUD_WPF_Test
+{
+    public class Pets_Integracao_Tests
+    {
+        private List<Pet> MockPets()
+        {
+            List<Pet> output = new List<Pet>
+            {
+                new Pet
+                {
+                    Id = 1,
+                    Nome = "Pet 1",
+                    Raca = "Raca 1",
+                    Cor = "Cor 1",
+                    Sexo = Sexo.Macho,
+                    Porte = Porte.Pequeno,
+                    Id_ong = 1,
+                },
+                new Pet
+                {
+                    Id = 2,
+                    Nome = "Pet 2",
+                    Raca = "Raca 2",
+                    Cor = "Cor 2",
+                    Sexo = Sexo.Fêmea,
+                    Porte = Porte.Médio,
+                    Id_ong = 2,
+                },
+                new Pet
+                {
+                    Id = 3,
+                    Nome = "Pet 3",
+                    Raca = "Raca 3",
+                    Cor = "Cor 3",
+                    Sexo = Sexo.Macho,
+                    Porte = Porte.Grande,
+                    Id_ong = 3,
+                }
+            };
+            return output;
+        }
+
+        [Fact]
+        public void BuscarPet()
+        {
+            Ong OngSelecionada = new Ong
+            {
+                Id = 1,
+                Nome = "Ong 1",
+                Endereco = "Endereço 1",
+                Telefone = "99999999999",
+                Email = "ong1@ong1",
+            };
+            Mock<IDatabase> mockDb = new Mock<IDatabase>();
+            mockDb.Setup(x => x.BuscaPets(OngSelecionada)).Returns(MockPets());
+
+            DataBaseGenerico conn = new DataBaseGenerico(mockDb.Object);
+
+            ObservableCollection<Pet> ListaPets = new ObservableCollection<Pet>(conn.BuscaPets(OngSelecionada));
+
+            var expected = MockPets();
+            var actual = ListaPets;
+
+            Assert.True(actual != null);
+            Assert.Equal(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Id, actual[i].Id);
+                Assert.Equal(expected[i].Nome, actual[i].Nome);
+                Assert.Equal(expected[i].Raca, actual[i].Raca);
+                Assert.Equal(expected[i].Cor, actual[i].Cor);
+                Assert.Equal(expected[i].Sexo, actual[i].Sexo);
+                Assert.Equal(expected[i].Porte, actual[i].Porte);
+                Assert.Equal(expected[i].Id_ong, actual[i].Id_ong);
+            }
+        }
+    }
+}
